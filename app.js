799be4bd03d7b4806d5597f00a41ff70016a5761
Atul <atulpratapsingh67@gmail.com>
2022-08-12 19:24:15 +0530

    First

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const mysql = require('mysql');
+
+const app = express();
+const port = 8080;
+
+
+const db = mysql.createConnection({
+    host:"newdatabase.cq3launkwzcx.ap-south-1.rds.amazonaws.com",
+    port:"3306",
+    user:"NewDatabase",
+    password:"NewDatabase"
+});
+
+const s="";
+db.connect(err=>{
+    if(err){
+        s=err;
+        console.log(err);
+        return;
+    }
+    s="Connected";
+    console.log("Connected");
+});
+
+
+app.get('/', (req, res) => res.send(s));
+
+app.listen(port);
+
+
+
