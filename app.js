befd329ae8f354c8290d5fc0ae26d3d659aeff41
Atul <atulpratapsingh67@gmail.com>
2022-08-12 21:58:19 +0530

    final

--- app.js
+++ app.js
@@ -1,30 +1,29 @@
 const express = require('express');
-const mysql = require('mysql');
+const mysql = require('mysql2');
 
 const app = express();
 const port = 8080;
 
 
-// const db = mysql.createConnection({
-//     host:"newdatabase.cq3launkwzcx.ap-south-1.rds.amazonaws.com",
-//     port:"3306",
-//     user:"NewDatabase",
-//     password:"NewDatabase"
-// });
+const db = mysql.createConnection({
+    host:"localhost",
+    port:"3306",
+    user:"root",
+    password:"Atul@12345",
+    database:"world"
+});
+let s="Not connected";
+db.connect(err=>{
+    if(err){
+        console.log(err);
+        return;
+    }
+    s="Connected";
+    console.log("Connected");
+});
 
-// const s="";
-// db.connect(err=>{
-//     if(err){
-//         s=err;
-//         console.log(err);
-//         return;
-//     }
-//     s="Connected";
-//     console.log("Connected");
-// });
 
-
-app.get('/', (req, res) => res.send("ss"));
+app.get('/', (req, res) => res.send(s));
 
 app.listen(port);
 
